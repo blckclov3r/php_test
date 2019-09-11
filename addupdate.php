95875a47175d74577cc391364898709af31a21f6
blckclov3r <blckclov3r@gmail.com>
2019-09-11 02:47:08 -0800

    add js & css

---
+++ addupdate.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Add Update</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        
+    </head>
+    <body>
+        <p>Hello world! This is HTML5 Boilerplate.</p>
+        
+    </body>
+</html>
--- index.php
+++ index.php
@@ -1,13 +1,25 @@
- <?php
- 	include 'db.php';
- ?> 
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+       
+    </head>
+    <body>
+        
+    	<div class="container-fluid">
+    		<center><h1>Employee List</h1></center>
 
- <!DOCTYPE html>
- <html>
- <head>
- 	<title>Helloworld</title>
- </head>
- <body>
- 	
- </body>
- </html>
+    		
+
+    	</div>
+
+        <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.js"></script>
+    </body>
+</html>
